e6ef0c4fc80877780e156705b3bce1cb57425ac6
JeongEun <lisasje25@gmail.com>
2021-06-21 17:42:20 +0900

    url routing_POST

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+
 app.listen(3000,function(){
     console.log("start! express server on port 3000");
 });
 
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+//url routing
 app.get('/', function(req,res){
     //res.send("<h1>hi friend</h1>")
     res.sendFile(__dirname + "/public/main.html")
-})
+});
+
+app.post('/email_post', function(req,res){
+    //get : req.param('email')
+    console.log(req.body) //객체{}로 넘어옴
+    console.log(req.body.email)//abc@naver.com 만 넘어옴
+    res.send("<h1>welcom " + req.body.email + "!</h1>")
+});
